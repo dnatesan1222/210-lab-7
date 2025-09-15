0f82e9244bdc9c78bc2d5aa0aa46f8ed93044ff8
Diksha <diksha@macbookpro>
2025-09-14 20:38:46 -0700

    complete with headers + reverseArray() bug fixed

--- moreArrays.cpp
+++ moreArrays.cpp
@@ -7,22 +7,31 @@ using namespace std;
 
 const int SIZE = 5;
 
+// reverseArray() reverses the elements of the received dynamic string array and returns a pointer to the modified string array w/o output
+// arguments: string *arr (a dynamic string array)
+// returns: a pointer to the reversed dynamic string array
 string* reverseArray(string *arr){
     string *reverse = nullptr;
     reverse = new string[SIZE];
     for (int i = 0; i < SIZE; i++){
-        *(reverse + i) = *(arr + (SIZE - i));
+        *(reverse + i) = *(arr + (SIZE - i - 1));
     }
     delete [] arr;
     return reverse;
 }
 
+// displayArray() outputs the elements of the received dynamic string array
+// arguments: string *arr (a dynamic string array)
+// returns: nothing
 void displayArray(string *arr){
     for (int i = 0; i < SIZE; i++)
         cout << *(arr + i) << " ";
     cout << endl;
 }
 
+// main() creates a dynamic string array and populates it with 5 names, then calls the functions to reverse and output the array
+// arguments: nothing
+// returns: nothing
 int main(){
     string *strArr = nullptr;
     strArr = new string[SIZE];
@@ -32,9 +41,9 @@ int main(){
     *(strArr+3) = "Ro";
     *(strArr+4) = "Zo";
     
-    cout << "OG array: ";
+    cout << "Original array: ";
     displayArray(strArr);
     strArr = reverseArray(strArr);
-    cout << "reversed array: ";
+    cout << "Reversed array: ";
     displayArray(strArr);
 }
