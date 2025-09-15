7f18d7330c84026170e863ed2c033b2b183977ca
Diksha <diksha@macbookpro>
2025-09-14 20:18:01 -0700

    reverseArray() started

--- moreArrays.cpp
+++ moreArrays.cpp
@@ -2,8 +2,20 @@
 // IDE used: Vim
 
 #include <iostream>
+#include <string>
 using namespace std;
 
+const int SIZE = 5;
+
+*string reverseArray(string *arr){
+    string *reverse = nullptr;
+    reverse = new string[SIZE];
+    for (int i = 0; i < SIZE; i++){
+        *(reverse + i) = *(arr + (SIZE - i));
+    }
+    delete [] arr;
+    return reverse;
+}
 
 int main(){
 
